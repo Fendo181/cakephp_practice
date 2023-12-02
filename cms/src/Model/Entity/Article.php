ee65c72f2180a98471f8bd8b1662442324b8718e
Fendo181 <fendo181git@gmail.com>
2023-12-02 16:04:43 +0900

    fix: 継承すべきファイルができてなかったので修正

--- cms/src/Model/Entity/Article.php
+++ cms/src/Model/Entity/Article.php
@@ -4,7 +4,7 @@ namespace App\Model\Entity;
 
 use Cake\ORM\Entity;
 
-class Article
+class Article extends Entity
 {
     protected $_accessible = [
         '*' => true,
--- cms/src/Model/Table/ArticlesTable.php
+++ cms/src/Model/Table/ArticlesTable.php
@@ -4,7 +4,7 @@ namespace App\Model\Table;
 
 use Cake\ORM\Table;
 
-class ArticlesTable
+class ArticlesTable extends Table
 {
     public function initialize(array $config): void
     {
