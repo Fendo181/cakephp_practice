c5ae52b22695a4d6102ad816b608dce27ca4f356
Fendo181 <fendo181git@gmail.com>
2023-12-02 16:25:52 +0900

    feat: 記事の詳細ページに飛べるようにviewメソッドを追加した

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -10,4 +10,12 @@ class ArticlesController extends AppController
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
+
+    // ユーザーが /articles/view/first-post をリクエストした際
+    // 値 'first-post' が CakePHP のルーティングとディスパッチレイヤーで $slug に渡されます。
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
 }
