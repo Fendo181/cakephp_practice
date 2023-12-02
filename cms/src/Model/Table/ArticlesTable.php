6e8d16240865e93b70efe58113e9a11effcb32c2
Fendo181 <fendo181git@gmail.com>
2023-12-02 19:16:24 +0900

    feat: tagを取得するカスタムファインダーメソッドを追加した

--- cms/src/Model/Table/ArticlesTable.php
+++ cms/src/Model/Table/ArticlesTable.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Table;
 
+use Cake\ORM\Query;
 use Cake\ORM\Table;
 use Cake\Utility\Text;
 use Cake\Event\EventInterface;
@@ -39,6 +40,36 @@ class ArticlesTable extends Table
                     ->minLength('body', 10);
 
         return $validator;
+    }
+
+    // $query 引数はクエリービルダーのインスタンスです。
+    // $options 配列には、コントローラーのアクションで find('tagged') に渡した
+    // "tags" オプションが含まれています。
+    public function findTagged(Query $query, array $options)
+    {
+        $coulumns = [
+            'Articles.id', 'Articles.user_id', 'Articles.title',
+            'Articles.body', 'Articles.published', 'Articles.created',
+            'Articles.slug',
+        ];
+
+        // distinctは、関連する記事が重複しないようにするために必要です。
+        $query = $query
+                    ->select($coulumns)
+                    ->distinct($coulumns);
+
+        if (empty($options['tags'])) {
+            // タグが指定していない場合は、タグのない記事を検索します。
+            $query->leftJoinWith('Tags')
+                    ->where(['Tags.title IS' => null]);
+        } else {
+            // 提供されたタグが1つ以上ある記事を検索します。
+            $query->innerJoinWith('Tags')
+                            ->where(['Tags.title IN' => $options['tags']]);
+        }
+
+        // 結果をタグでグループ化します。
+        return $query->group('Articles.id');
     }
 
 }
