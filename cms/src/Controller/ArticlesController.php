d88d8766eadb202c6be79c1d063b5143c4e416ce
Fendo181 <fendo181git@gmail.com>
2023-12-03 11:11:29 +0900

    feat: 記事を編集する際にtagも取得できるようにした

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -63,7 +63,11 @@ class ArticlesController extends AppController
 
     public function edit($slug)
     {
-        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $article = $this->Articles
+                        ->findBySlug($slug)
+                        ->contain('Tags')
+                        ->firstOrFail();
+
         if ($this->request->is(['post','put'])) {
             $this->Articles->patchEntity($article, $this->request->getData());
             if ($this->Articles->save($article)) {
