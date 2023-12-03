c873b60f5515b133ad0d3a3ef01ffaf2e353ba9b
Fendo181 <fendo181git@gmail.com>
2023-12-03 11:55:11 +0900

    feat: 認証を行うために認証用のインターフェースを追加します

--- cms/src/Application.php
+++ cms/src/Application.php
@@ -27,6 +27,14 @@ use Cake\Http\MiddlewareQueue;
 use Cake\ORM\Locator\TableLocator;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
+// 認証で必要なクラスをuseする
+use Authentication\AuthenticationService;
+use Authentication\AuthenticationServiceInterface;
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
+use Cake\Routing\Route\Route;
+use Cake\Routing\Router;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Application setup class.
@@ -34,7 +42,7 @@ use Cake\Routing\Middleware\RoutingMiddleware;
  * This defines the bootstrapping logic and middleware layers you
  * want to use in your application.
  */
-class Application extends BaseApplication
+class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
     /**
      * Load all the application configuration and bootstrap logic.
@@ -90,6 +98,9 @@ class Application extends BaseApplication
             // See https://github.com/CakeDC/cakephp-cached-routing
             ->add(new RoutingMiddleware($this))
 
+            // 認証を行うミドルウェアを追加
+            ->add(new AuthenticationMiddleware($this))
+
             // Parse various types of encoded request bodies so that they are
             // available as array through $request->getData()
             // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
@@ -102,6 +113,37 @@ class Application extends BaseApplication
             ]));
 
         return $middlewareQueue;
+    }
+
+    public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface
+    {
+        // AuthenticationServiceのインスタンスを作成
+        $authenticationService = new AuthenticationService([
+            'unauthenticatedRedirect' => Router::url('/users/login'),
+            'queryParam' => 'redirect',
+        ]);
+
+        // 認証に使用する識別子ローダーを作成
+        $authenticationService->loadIdentifier('Authentication.password', [
+            'fields' => [
+                'username' => 'email',
+                'password' => 'password',
+            ],
+        ]);
+
+        // 認証に使用するレスポンダーを作成
+        $authenticationService->loadAuthenticator('Authentication.Session');
+
+        // フォーム認証を追加
+        $authenticationService->loadAuthenticator('Authentication.Form', [
+           'fields' => [
+               'username' => 'email',
+               'password' => 'password',
+           ],
+            'loginUrl' => Router::url('users/login'),
+        ]);
+
+        return  $authenticationService;
     }
 
     /**
