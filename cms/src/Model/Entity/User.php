df69e45a0fcc69a1bb23d8912a4374974e4c1f12
Fendo181 <fendo181git@gmail.com>
2023-12-03 11:53:12 +0900

    feat: パスワードを保存する前にミューテーターを使って、パスワードをハッシュ化する

--- cms/src/Model/Entity/User.php
+++ cms/src/Model/Entity/User.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
+use Authentication\PasswordHasher\DefaultPasswordHasher;
 use Cake\ORM\Entity;
 
 /**
@@ -43,4 +44,14 @@ class User extends Entity
     protected $_hidden = [
         'password',
     ];
+
+
+    // __setPasswordはミューテーター/セッターメソッドです。
+    // このメソッドは、ユーザーがパスワードを送信したときに呼び出されます
+    protected function _setPassword(string $password) : ?string
+    {
+        if (strlen($password) > 0) {
+            return (new DefaultPasswordHasher())->hash($password);
+        }
+    }
 }
